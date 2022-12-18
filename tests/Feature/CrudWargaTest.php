0734b32d32dadc253746db381b18f8617997a9ff
hrzaa <harezayoan@gmail.com>
2022-12-18 15:59:25 +0700

    add CrudTest - see edit page

--- tests/Feature/CrudWargaTest.php
+++ tests/Feature/CrudWargaTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Warga;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -25,5 +26,14 @@ class CrudWargaTest extends TestCase
 
         $response->assertRedirect('/warga');
         $this->assertDatabaseHas('warga', ['nama' => 'Dinar', 'nik' => '66666666', 'email' => 'dinar@gmail.com', 'jenis_kelamin' => 'P', 'alamat' => 'Karanganyar']);
+    }
+
+    public function test_user_can_see_edit_page()
+    {
+        $user = User::factory()->create();
+        $warga = Warga::factory()->create();
+        $response = $this->actingAs($user)->get('/warga/edit/' . $warga->id);
+        $response->assertStatus(200);
+        $response->assertSee($warga->nama);
     }
 }
